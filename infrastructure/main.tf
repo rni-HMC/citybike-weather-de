dff5f59df68cd60a9bf8351f93ca5ffad4dfa0e3
Richard Ni <rni@hmc.edu>
2024-05-10 04:00:09 +0000

    Change ubuntu image to 22.04

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,20 +5,19 @@ terraform {
       version = "~> 4.16"
     }
   }
- 
+
   required_version = ">= 1.2.0"
 }
- 
+
 provider "aws" {
   region = "us-west-2"
 }
- 
 resource "aws_instance" "app_server" {
-  ami                    = "ami-08d70e59c07c61a3a"
+  ami                    = "ami-09c3a3c2cf6003f6c"
   instance_type          = "t2.micro"
   key_name               = "aws_key"
   vpc_security_group_ids = [aws_security_group.main.id]
- 
+
   tags = {
     Name = "CityBikeWeatherAppServerInstance"
   }
@@ -30,7 +29,7 @@ resource "aws_instance" "app_server" {
     timeout     = "4m"
   }
 }
- 
+
 resource "aws_security_group" "main" {
   egress = [
     {
@@ -59,8 +58,8 @@ resource "aws_security_group" "main" {
     }
   ]
 }
- 
- 
+
+
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDaH3M8Zj5wIgzkAfqEBsG053t4Sujt3FHmi7rlb7DXQmnlANmPeXL4QK15xHJZWjhynyKb/7Al65I8Elr86qMY6zXFLN0+Me08lkmNexbXoDOhkXRld3JE5G6hZlbJZQFJen8KaKqDe8hGbHaXNgpS0GGB3yLRBe+QpTduD4whB3QKvYvyE5OVkUmUSmwPuswdCGdhAx+Bkyt245mKFVzwtIsvGD3DFw6pqcSXTv+EnLADcvLbUCvh2/b1ISFJX7ajhBkvJw+AxFo5KJKo9dQ05QaL5OcEK3oLgvndodog6BTg8jU3+fzshqOzMiirTKa6r6ih4hokLEwL6dGDjOPX richard@l-richni-02"
